0fd0f92094cc53a15ee5a9339fb440844e716949
youngauto <from_yangxin@126.com>
2017-05-31 16:11:26 +0800

    一些修改

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentWillMount() {
+      console.warn(123);
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="bg-content">
         </div>
         <p className="App-intro">
+          <img src={logo} className="App-logo" alt="logo" />
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
